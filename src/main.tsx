de33ef48d3b696cec562e95a875596bbc7293999
lord-navy-crypto <haojunli283@gmail.com>
2026-09-01 11:39:45 +0800

    feat: mount Smart Lab and official model library

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import Diagnostics from './Diagnostics';
+import SmartLab from './SmartLab';
 import './styles.css';
+import './smartlab.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><App /><Diagnostics /></React.StrictMode>
+  <React.StrictMode><App /><Diagnostics /><SmartLab /></React.StrictMode>
 );
