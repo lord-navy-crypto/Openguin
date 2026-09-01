209a645b04fd4123226445ab80f8bccd85d712e3
lord-navy-crypto <haojunli283@gmail.com>
2026-09-01 21:32:35 +0800

    fix: use taskBus event module

--- src/MegaLibrary.tsx
+++ src/MegaLibrary.tsx
@@ -1,6 +1,6 @@
 import {useEffect,useMemo,useState} from 'react';
 import {invoke} from '@tauri-apps/api/core';
-import {task} from './taskCenter';
+import {task} from './taskBus';
 import './mega-library.css';
 
 type Mode='bundled'|'external';
