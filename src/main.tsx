d4e271ebd93d2d65f21db680a49666787813c152
lord-navy-crypto <haojunli283@gmail.com>
2026-09-01 20:53:28 +0800

    feat: mount global task center

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App07 from './App07';
+import TaskCenter from './TaskCenter';
 
 const sessions=Number(localStorage.getItem('modeldock-sessions')||'0')+1;
 localStorage.setItem('modeldock-sessions',String(sessions));
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App07/></React.StrictMode>);
+ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App07/><TaskCenter/></React.StrictMode>);
